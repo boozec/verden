58d6870c33d664157c3efc9c75686e6c0779a96f
Santo Cariotti <santo@dcariotti.me>
2022-09-10 13:04:19 +0200

    Drop `Debug` from Pagination

--- src/pagination.rs
+++ src/pagination.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize)]
 pub struct Pagination {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub page: Option<i64>,
