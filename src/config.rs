73cf44e4881c5f75e21dcd8196e03cb3f8edcee0
Santo Cariotti <santo@dcariotti.me>
2022-09-12 15:50:14 +0200

    Remove old config

--- src/config.rs
+++ src/config.rs
@@ -2,14 +2,6 @@ pub use config::ConfigError;
 use lazy_static::lazy_static;
 use serde::Deserialize;
 
-// pub static PAGE_LIMIT: i64 = std::env::var("PAGE_LIMIT")
-//     .unwrap_or_else(|_| "20".to_string())
-//     .parse::<i64>()
-//     .unwrap();
-// pub const MAX_UPLOAD_FILE_SIZE: u64 = 1024 * 1024; // 1 MB
-// pub const SAVE_FILE_BASE_PATH: &str = &std::env::var("SAVE_FILE_BASE_PATH").unwrap();
-// pub const UPLOADS_ENDPOINT: &str = &std::env::var("UPLOADS_ENDPOINT").unwrap();
-
 #[derive(Deserialize)]
 pub struct Configuration {
     pub page_limit: i64,
